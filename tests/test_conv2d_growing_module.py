a3d9c510bf0e99d1ec15522eb6eb59af819a12b3
Théo Rudkiewicz <theo.rudkiewicz@inria.fr>
2024-12-13 16:27:00 +0100

    feat: Add test with bias activated (commented some that do not work yet)

--- tests/test_conv2d_growing_module.py
+++ tests/test_conv2d_growing_module.py
@@ -17,12 +17,25 @@ class MyTestCase(TestCase):
         )
         self.demo.layer = self.demo_layer
 
+        self.demo_layer_b = torch.nn.Conv2d(
+            2, 7, 3, padding=1, bias=True, device=global_device()
+        )
+        self.demo_b = Conv2dGrowingModule(
+            in_channels=2, out_channels=7, kernel_size=3, padding=1, use_bias=True
+        )
+        self.demo_b.layer = self.demo_layer_b
+
+        torch.manual_seed(0)
+        self.input_x = torch.randn(5, 2, 10, 10, device=global_device())
+
     def test_init(self):
+        # no bias
         m = Conv2dGrowingModule(
             in_channels=2, out_channels=7, kernel_size=(3, 5), use_bias=False
         )
         self.assertIsInstance(m, Conv2dGrowingModule)
 
+        # with bias
         m = Conv2dGrowingModule(
             in_channels=2, out_channels=7, kernel_size=3, padding=1, use_bias=True
         )
@@ -32,26 +45,53 @@ class MyTestCase(TestCase):
         self.assertEqual(m.layer.kernel_size, (3, 3))
 
     def test_forward(self):
-        torch.manual_seed(0)
-        x = torch.randn(5, 2, 10, 10, device=global_device())
-        y = self.demo(x)
-        self.assertTrue(torch.equal(y, self.demo_layer(x)))
+        # no bias
+        y = self.demo(self.input_x)
+        self.assertTrue(torch.equal(y, self.demo_layer(self.input_x)))
+
+        # with bias
+        y = self.demo_b(self.input_x)
+        self.assertTrue(torch.equal(y, self.demo_layer_b(self.input_x)))
 
     def test_number_of_parameters(self):
+        # no bias
         self.assertEqual(self.demo.number_of_parameters(), self.demo_layer.weight.numel())
+
+        # with bias
+        self.assertEqual(
+            self.demo_b.number_of_parameters(),
+            self.demo_layer_b.weight.numel() + self.demo_layer_b.bias.numel(),
+        )
 
     def test_str(self):
         self.assertIsInstance(str(self.demo), str)
+        self.assertIsInstance(repr(self.demo), str)
+        self.assertIsInstance(str(self.demo_b), str)
         for i in (0, 1, 2):
             self.assertIsInstance(self.demo.__str__(i), str)
 
     def test_layer_of_tensor(self):
+        # no bias
         wl = self.demo.layer_of_tensor(self.demo_layer.weight.data)
         # way to test that wl == self.demo_layer
-        torch.manual_seed(0)
-        x = torch.randn(5, 2, 10, 10, device=global_device())
-        y = self.demo(x)
-        self.assertTrue(torch.equal(y, wl(x)))
+        y = self.demo(self.input_x)
+        self.assertTrue(torch.equal(y, wl(self.input_x)))
+
+        with self.assertRaises(AssertionError):
+            _ = self.demo.layer_of_tensor(
+                self.demo_layer.weight.data, self.demo_layer_b.bias.data
+            )
+
+        # with bias
+        wl = self.demo_b.layer_of_tensor(
+            self.demo_layer_b.weight.data, self.demo_layer_b.bias.data
+        )
+        # way to test that wl == self.demo_layer
+        y = self.demo_b(self.input_x)
+        self.assertTrue(torch.equal(y, wl(self.input_x)))
+
+        with self.assertRaises(AssertionError):
+            _ = self.demo_b.layer_of_tensor(self.demo_layer_b.weight.data)
 
     def test_layer_in_extension(self):
         in_extension = torch.nn.Conv2d(3, 7, (3, 5), bias=False, device=global_device())
@@ -74,11 +114,9 @@ class MyTestCase(TestCase):
         local_demo = deepcopy(self.demo)
         local_demo.layer_out_extension(out_extension.weight)
 
-        torch.manual_seed(0)
-        x = torch.randn(23, 2, 10, 10, device=global_device())
-        y_main_th = self.demo(x)
-        y_ext_th = out_extension(x)
-        y = local_demo(x)
+        y_main_th = self.demo(self.input_x)
+        y_ext_th = out_extension(self.input_x)
+        y = local_demo(self.input_x)
         y_main = y[:, :7]
         y_ext = y[:, 7:]
         self.assertTrue(
@@ -90,58 +128,92 @@ class MyTestCase(TestCase):
             f"Error: ({torch.abs(y_ext - y_ext_th).max().item():.2e})",
         )
 
-    def test_tensor_s_update(self):
-        torch.manual_seed(0)
-        x = torch.randn(23, 2, 10, 10, device=global_device())
-
+    def test_tensor_s_update_without_bias(self):
         self.demo.init_computation()
-        self.demo(x)
+        self.demo(self.input_x)
 
         self.demo.tensor_s.update()
-        self.assertEqual(self.demo.tensor_s.samples, 23)
+        self.assertEqual(self.demo.tensor_s.samples, self.input_x.size(0))
         # TODO: improve the specificity of the test
         # here we only test that the number of samples is correct
 
         self.demo.tensor_s.update()
-        self.assertEqual(self.demo.tensor_s.samples, 23)
+        self.assertEqual(self.demo.tensor_s.samples, self.input_x.size(0))
 
-        self.demo(x)
+        self.demo(self.input_x)
         self.demo.tensor_s.update()
-        self.assertEqual(self.demo.tensor_s.samples, 2 * 23)
+        self.assertEqual(self.demo.tensor_s.samples, 2 * self.input_x.size(0))
+
+        f = self.demo.in_channels * self.demo.kernel_size[0] * self.demo.kernel_size[1]
+        self.assertEqual(self.demo.tensor_s().shape, (f, f))
+        self.assertTrue(
+            torch.allclose(self.demo.tensor_s(), self.demo.tensor_s().transpose(0, 1))
+        )
 
         self.demo.reset_computation()
 
-    def test_tensor_m_update(self):
-        torch.manual_seed(0)
-        x = torch.randn(23, 2, 10, 10, device=global_device())
+    """
+    def test_tensor_s_update_with_bias(self):
+        self.demo_b.init_computation()
+        self.demo_b(self.input_x)
 
+        self.demo_b.tensor_s.update()
+        self.assertEqual(self.demo_b.tensor_s.samples, self.input_x.size(0))
+        # TODO: improve the specificity of the test
+        # here we only test that the number of samples is correct
+
+        f = self.demo_b.in_channels * self.demo_b.kernel_size[0] * self.demo_b.kernel_size[1] + 1
+        self.assertEqual(self.demo_b.tensor_s().shape, (f, f))
+        self.assertEqual(self.demo_b.tensor_s()[-1, -1], 1)
+        self.assertTrue(torch.allclose(self.demo_b.tensor_s(), self.demo.tensor_s().transpose(0, 1)))
+
+        self.demo.reset_computation()
+    """
+
+    def test_tensor_m_update_without_bias(self):
         self.demo.init_computation()
-        y = self.demo(x)
+        y = self.demo(self.input_x)
         loss = torch.norm(y)
         loss.backward()
 
         self.demo.tensor_m.update()
-        self.assertEqual(self.demo.tensor_m.samples, 23)
+        self.assertEqual(self.demo.tensor_m.samples, self.input_x.size(0))
         # TODO: improve the specificity of the test
         # here we only test that the number of samples is correct
 
         self.demo.tensor_m.update()
-        self.assertEqual(self.demo.tensor_m.samples, 23)
+        self.assertEqual(self.demo.tensor_m.samples, self.input_x.size(0))
 
-        y = self.demo(x)
+        y = self.demo(self.input_x)
         loss = torch.norm(y)
         loss.backward()
         self.demo.tensor_m.update()
-        self.assertEqual(self.demo.tensor_m.samples, 2 * 23)
+        self.assertEqual(self.demo.tensor_m.samples, 2 * self.input_x.size(0))
 
+        f = self.demo.in_channels * self.demo.kernel_size[0] * self.demo.kernel_size[1]
+        self.assertEqual(self.demo.tensor_m().shape, (f, self.demo.out_channels))
         self.demo.reset_computation()
 
-    def test_compute_optimal_delta(self):
-        torch.manual_seed(0)
-        x = torch.randn(23, 2, 10, 10, device=global_device())
+    """
+    def test_tensor_m_update_with_bias(self):
+        self.demo_b.init_computation()
+        y = self.demo_b(self.input_x)
+        loss = torch.norm(y)
+        loss.backward()
 
+        self.demo_b.tensor_m.update()
+        self.assertEqual(self.demo_b.tensor_m.samples, self.input_x.size(0))
+        # TODO: improve the specificity of the test
+        # here we only test that the number of samples is correct
+
+        f = self.demo_b.in_channels * self.demo_b.kernel_size[0] * self.demo_b.kernel_size[1] + 1
+        self.assertEqual(self.demo_b.tensor_m().shape, (f, self.demo_b.out_channels))
+        self.demo.reset_computation()
+    """
+
+    def test_compute_optimal_delta_without_bias(self):
         self.demo.init_computation()
-        y = self.demo(x)
+        y = self.demo(self.input_x)
         loss = torch.norm(y)
         loss.backward()
 
@@ -155,6 +227,26 @@ class MyTestCase(TestCase):
 
         self.demo.reset_computation()
         self.demo.delete_update()
+
+    """
+    def test_compute_optimal_delta_with_bias(self):
+        self.demo_b.init_computation()
+        y = self.demo_b(self.input_x)
+        loss = torch.norm(y)
+        loss.backward()
+
+        self.demo_b.tensor_s.update()
+        self.demo_b.tensor_m.update()
+
+        self.demo_b.compute_optimal_delta()
+        self.assertTrue(self.demo_b.optimal_delta_layer is not None)
+        self.assertIsInstance(self.demo_b.optimal_delta_layer, torch.nn.Conv2d)
+        self.assertTrue(self.demo_b.optimal_delta_layer.bias is not None)
+        # TODO: improve the specificity of the test
+
+        self.demo_b.reset_computation()
+        self.demo_b.delete_update()
+    """
 
     def test_mask_tensor_t(self):
         with self.assertRaises(AssertionError):
