666a543e0205c512ab3e2ce6705f9ce01ef93483
streethagore <srivaud972@gmail.com>
2024-12-16 19:35:28 +0100

    Trying jobs on cluster

--- scripts/create_conda_env.sh
+++ scripts/create_conda_env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-conda create -n gromo python=3.12
+#conda create -n gromo python=3.12
 
-conda init
+#conda init
 conda activate gromo
 
 # install dependencies
-pip install -e .
+#pip install -e .
 
 # testing the pytorch installation and the GPU
-python -c "import torch; print(torch.cuda.is_available())"
+#python -c "import torch; print(torch.cuda.is_available())"
