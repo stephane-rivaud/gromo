83bd0b21424c59be9a081d664683506292e8e1f9
streethagore <srivaud972@gmail.com>
2024-12-17 16:33:02 +0100

    Trying jobs on cluster

--- scripts/mlflow_remote_server.sh
+++ scripts/mlflow_remote_server.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # connect to the remote server
-ssh -L 5000:127.0.0.1:5000 strivaud@ssh.saclay.inria.fr
-cd /home/tau/strivaud/gromo || { echo "Failed to change directory"; exit 1; }
+port=6000
+ssh -L $port:127.0.0.1:$port strivaud@ssh.saclay.inria.fr
+cd /home/tau/strivaud/gromo || {
+  echo "Failed to change directory"
+  exit 1
+}
 
 # launching the mlflow server
 backend_store_uri="logs/mlp_run"
@@ -10,4 +14,5 @@ artifact_store_uri="logs/mlp_run"
 mlflow server \
   --backend-store-uri $backend_store_uri \
   --default-artifact-root $artifact_store_uri \
-  --host
+  --host 127.0.0.1 \
+  --port $port
