7fa3c6f1aaee95a3dc97a3be0958a114b81fc895
streethagore <srivaud972@gmail.com>
2024-12-17 16:36:15 +0100

    Trying jobs on cluster

--- scripts/mlflow_remote_server.sh
+++ scripts/mlflow_remote_server.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# connect to the remote server
-port=6000
-ssh -L $port:127.0.0.1:$port strivaud@ssh.saclay.inria.fr
-ssh -A $port:127.0.0.1:$port titanic
-cd /home/tau/strivaud/gromo || {
-  echo "Failed to change directory"
-  exit 1
-}
+# Define the port to use for port forwarding
+local_port=6000
+remote_port=6000
 
-# launching the mlflow server
-backend_store_uri="logs/mlp_run"
-artifact_store_uri="logs/mlp_run"
-mlflow server \
-  --backend-store-uri $backend_store_uri \
-  --default-artifact-root $artifact_store_uri \
-  --host 127.0.0.1 \
-  --port $port
+# Connect to the remote server with port forwarding
+ssh -L $local_port:127.0.0.1:$remote_port strivaud@ssh.saclay.inria.fr << 'EOF'
+  # Connect to the titanic server
+  ssh -L $remote_port:127.0.0.1:$remote_port titanic << 'EOT'
+    # Navigate to the project directory
+    cd /home/tau/strivaud/gromo || {
+      echo "Failed to change directory"
+      exit 1
+    }
+
+    # Launch the mlflow server
+    backend_store_uri="logs/mlp_run"
+    artifact_store_uri="logs/mlp_run"
+    mlflow server \
+      --backend-store-uri $backend_store_uri \
+      --default-artifact-root $artifact_store_uri \
+      --host 127.0.0.1 \
+      --port $remote_port
+  EOT
+EOF
