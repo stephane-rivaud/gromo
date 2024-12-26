5c7acb5f7069432afb13dfc771ea1b09a36f446e
streethagore <srivaud972@gmail.com>
2024-12-26 15:10:59 +0100

    Trying jobs on cluster

--- scripts/mlp_launcher.sh
+++ scripts/mlp_launcher.sh
@@ -20,7 +20,7 @@ for nb_hidden_layer in 1 2 3 4; do
         for selection_method in "none" "fo" "scaled_fo" "one_step_fo"; do
           command="scripts/mlp_run.sh $nb_hidden_layer $hidden_size $weight_decay $epoch_per_growth $selection_method"
           echo $command
-          sbtach --gres=gpu:1 --time=00:45:00 $command
+          sbatch --gres=gpu:1 --time=00:45:00 $command
         done
       done
     done
