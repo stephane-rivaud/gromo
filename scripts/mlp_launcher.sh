a0f2fe384b4f72dca6c2e7407dc16d41cfa7a53f
streethagore <srivaud972@gmail.com>
2024-12-26 15:08:32 +0100

    Trying jobs on cluster

--- scripts/mlp_launcher.sh
+++ scripts/mlp_launcher.sh
@@ -11,16 +11,16 @@ weight_decay=0
 
 # growth arguments
 epoch_per_growth=10
-selection_method="none" # "none", "fo"
+selection_method="none" # "none", "fo", "scaled_fo", "one_step_fo"
 
 for nb_hidden_layer in 1 2 3 4; do
   for hidden_size in 8 16 32; do
-    for weight_decay in 0 0.0001 0.001; do
-      for epoch_per_growth in -1 1 2 3 4; do
-        for selection_method in "none" "fo"; do
-          command="scripts/mlp_run.sh $nb_hidden_layer $hidden_size $weight_decay $epoch_per_growth $selection_method"
+    for weight_decay in 0 0.0001 0.001 0.01; do
+      for epoch_per_growth in 4 8 16; do
+        for selection_method in "none" "fo" "scaled_fo" "one_step_fo"; do
+          command="mlp_run.sh $nb_hidden_layer $hidden_size $weight_decay $epoch_per_growth $selection_method"
           echo $command
-          sbatch --gres=gpu:1 --time=04:00:00 --partition titanic $command
+          sbtach --gres=gpu:1 --time=00:45:00 $command
         done
       done
     done
