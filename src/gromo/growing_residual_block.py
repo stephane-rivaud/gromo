bd15c22894db374bd0bd871a479448907fc13136
streethagore <srivaud972@gmail.com>
2025-01-03 13:13:11 +0100

    Implementing GrowingResidualBlock

--- src/gromo/growing_residual_block.py
+++ src/gromo/growing_residual_block.py
@@ -4,10 +4,7 @@ Module to define a two layer block similar to a BasicBlock in ResNet.
 
 import torch
 
-from gromo.growing_module import AdditionGrowingModule, GrowingModule
 from gromo.linear_growing_module import LinearAdditionGrowingModule, LinearGrowingModule
-from gromo.tensor_statistic import TensorStatistic
-
 
 all_layer_types = {
     "linear": {"layer": LinearGrowingModule, "addition": LinearAdditionGrowingModule},
@@ -26,13 +23,13 @@ class GrowingResidualBlock(torch.nn.Module):
     """
 
     def __init__(
-        self,
-        in_out_features: int,
-        hidden_features: int = 0,
-        layer_type: str = "linear",
-        activation: torch.nn.Module | None = None,
-        name: str = "block",
-        kwargs_layer: dict | None = None,
+            self,
+            in_out_features: int,
+            hidden_features: int = 0,
+            layer_type: str = "linear",
+            activation: torch.nn.Module | None = None,
+            name: str = "block",
+            kwargs_layer: dict | None = None,
     ) -> None:
         """
         Initialise the block.
@@ -97,28 +94,6 @@ class GrowingResidualBlock(torch.nn.Module):
     def scaling_factor(self):
         return self.second_layer.scaling_factor
 
-    @staticmethod
-    def set_default_values(
-        activation: torch.nn.Module | None = None,
-        pre_activation: torch.nn.Module | None = None,
-        mid_activation: torch.nn.Module | None = None,
-        kwargs_layer: dict | None = None,
-        kwargs_first_layer: dict | None = None,
-        kwargs_second_layer: dict | None = None,
-    ) -> tuple[torch.nn.Module | None, torch.nn.Module | None, dict | None, dict | None]:
-        """
-        Set default values for the block.
-        """
-        if pre_activation is None:
-            pre_activation = activation
-        if mid_activation is None:
-            mid_activation = activation
-        if kwargs_first_layer is None:
-            kwargs_first_layer = kwargs_layer
-        if kwargs_second_layer is None:
-            kwargs_second_layer = kwargs_layer
-        return pre_activation, mid_activation, kwargs_first_layer, kwargs_second_layer
-
     def extended_forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Forward pass of the block with the current modifications.
@@ -138,7 +113,7 @@ class GrowingResidualBlock(torch.nn.Module):
             y, y_ext = self.first_layer.extended_forward(x)
             y, _ = self.second_layer.extended_forward(y, y_ext)
             assert (
-                _ is None
+                    _ is None
             ), f"The output of layer 2 {self.second_layer.name} should not be extended."
             del y_ext
             x = y + x
@@ -194,16 +169,6 @@ class GrowingResidualBlock(torch.nn.Module):
         """
         Initialise the computation of the block.
         """
-        # self.first_layer.store_input = True
-        # self.first_layer.store_pre_activity = True
-        # self.first_layer.tensor_s.init()
-        # self.first_layer.tensor_m.init()
-        # self.second_layer.store_input = True
-        # self.second_layer.store_pre_activity = True
-        # self.second_layer.tensor_s.init()
-        # self.second_layer.tensor_m.init()
-        # self.second_layer.tensor_m_prev.init()
-        # self.second_layer.cross_covariance.init()
         self.first_layer.init_computation()
         self.second_layer.init_computation()
 
@@ -216,10 +181,6 @@ class GrowingResidualBlock(torch.nn.Module):
         desired_activation: torch.Tensor
             desired direction of the output variation of the block
         """
-        # self.second_layer.tensor_m.update(desired_activation=desired_activation)
-        # self.second_layer.tensor_s.update()
-        # self.second_layer.tensor_m_prev.update(desired_activation=desired_activation)
-        # self.second_layer.cross_covariance.update()
         self.first_layer.update_computation()
         self.second_layer.update_computation()
 
@@ -227,26 +188,20 @@ class GrowingResidualBlock(torch.nn.Module):
         """
         Reset the computation of the block.
         """
-        self.first_layer.store_input = False
-        self.second_layer.store_input = False
-        self.second_layer.tensor_s.reset()
-        self.second_layer.tensor_m.reset()
-        self.second_layer.tensor_m_prev.reset()
-        self.second_layer.cross_covariance.reset()
+        self.first_layer.reset_computation()
+        self.second_layer.reset_computation()
 
     def delete_update(self):
         """
         Delete the update of the block.
         """
-        self.second_layer.optimal_delta_layer = None
-        self.second_layer.extended_input_layer = None
-        self.first_layer.extended_input_layer = None
+        self.second_layer.delete_update(include_previous=True)
 
     def compute_optimal_updates(
-        self,
-        numerical_threshold: float = 1e-15,
-        statistical_threshold: float = 1e-3,
-        maximum_added_neurons: int | None = None,
+            self,
+            numerical_threshold: float = 1e-15,
+            statistical_threshold: float = 1e-3,
+            maximum_added_neurons: int | None = None,
     ) -> None:
         """
         Compute the optimal update for second layer and additional neurons.
@@ -266,10 +221,8 @@ class GrowingResidualBlock(torch.nn.Module):
                 numerical_threshold=numerical_threshold,
                 statistical_threshold=statistical_threshold,
                 maximum_added_neurons=maximum_added_neurons,
+                update_previous=True,
             )
-        )
-        self.first_layer.extended_output_layer = self.first_layer.layer_of_tensor(
-            alpha, alpha_bias
         )
 
     def apply_change(self) -> None:
@@ -277,13 +230,12 @@ class GrowingResidualBlock(torch.nn.Module):
         Apply the optimal delta and extend the layer with current
         optimal delta and layer extension with the current scaling factor.
         """
-        self.first_layer.apply_change()
-        self.second_layer.apply_change()
+        self.second_layer.apply_change(apply_previous=True)
         self.hidden_features += self.eigenvalues.shape[0]
 
     def sub_select_optimal_added_parameters(
-        self,
-        keep_neurons: int,
+            self,
+            keep_neurons: int,
     ) -> None:
         """
         Select the first keep_neurons neurons of the optimal added parameters.
@@ -294,8 +246,7 @@ class GrowingResidualBlock(torch.nn.Module):
             number of neurons to keep
         """
         self.eigenvalues = self.eigenvalues[:keep_neurons]
-        # self.first_layer.sub_select_optimal_added_parameters(keep_neurons)
-        self.second_layer.sub_select_optimal_added_parameters(keep_neurons)
+        self.second_layer.sub_select_optimal_added_parameters(keep_neurons, sub_select_previous=True)
 
     @property
     def first_order_improvement(self) -> torch.Tensor:
@@ -308,57 +259,134 @@ class GrowingResidualBlock(torch.nn.Module):
             first order improvement
         """
         return (
-            self.parameter_update_decrease
-            + self.activation_derivative * (self.eigenvalues**2).sum()
+                self.parameter_update_decrease
+                + self.activation_derivative * (self.eigenvalues ** 2).sum()
         )
 
 
 if __name__ == "__main__":
-    # Test the GrowingResidualBlock
     import torch
     import torch.nn as nn
     import torch.nn.functional as F
 
+    torch.manual_seed(0)
+
+    reduction = "mean"
+
+
+    def train(model, dataset, optimizer):
+        total_loss = 0
+        for x, y in dataset:
+            optimizer.zero_grad()
+            y_hat = model(x)
+            loss = F.mse_loss(y_hat, y, reduction=reduction)
+            total_loss += loss
+            loss.backward()
+            optimizer.step()
+        return total_loss / len(dataset)
+
+
+    def evaluate(model, dataset):
+        total_loss = 0
+        with torch.no_grad():
+            for x, y in dataset:
+                y_hat = model(x)
+                loss = F.mse_loss(y_hat, y, reduction=reduction)
+                total_loss += loss
+        return total_loss / len(dataset)
+
+
+    def compute_statistics(model, dataset):
+        model.init_computation()
+        total_loss = 0
+        for x, y in dataset:
+            y_hat = model(x)
+            loss = F.mse_loss(y_hat, y, reduction="sum")
+            total_loss += loss
+            loss.backward()
+            model.update_computation()
+        return total_loss / len(dataset)
+
+
+    def evaluate_with_extension(model, dataset, scaling_factor):
+        model.second_layer.scaling_factor = scaling_factor
+        total_loss = 0
+        with torch.no_grad():
+            for x, y in dataset:
+                y_hat = model.extended_forward(x)
+                loss = F.mse_loss(y_hat, y, reduction=reduction)
+                total_loss += loss
+        return total_loss / len(dataset)
+
+
     # Define the dataset
-    batch_size = 2
-    num_batch = 4
+    batch_size = 100
+    num_batch = 10
     in_features = 10
     dataset = [(torch.randn(batch_size, in_features), torch.randn(batch_size, in_features)) for _ in range(num_batch)]
 
     # Define the block
-    hidden_features = 8
+    hidden_features = 2
     block = GrowingResidualBlock(in_features, hidden_features, activation=nn.ReLU(), name="block")
     print(block)
 
     # Define the optimizer
-    optimizer = torch.optim.SGD(block.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-3)
+    optimizer = torch.optim.SGD(block.parameters(), lr=0.01 / num_batch, momentum=0.9, weight_decay=1e-3)
 
     # Regular training
-    for x, y in dataset:
-        optimizer.zero_grad()
-        y_hat = block(x)
-        loss = F.mse_loss(y_hat, y)
-        loss.backward()
-        optimizer.step()
-    print(f'Loss: {loss}')
+    for epoch in range(42):
+        training_loss = train(block, dataset, optimizer)
+        print(f'Epoch {epoch}, Training Loss: {training_loss}')
+
+    # Training loss after training
+    training_loss_after = evaluate(block, dataset)
+    print(f'Training Loss after training: {training_loss_after}')
 
     # Gathering growing statistics
-    block.init_computation()
-    for x, y in dataset:
-        y_hat = block(x)
-        loss = F.mse_loss(y_hat, y, reduction='sum')
-        loss.backward()
-        block.update_computation()
+    statistics_loss = compute_statistics(block, dataset)
+    print(f'Training Loss after gathering statistics: {statistics_loss}')
 
     # Compute the optimal update
-    keep_neurons = 3
+    keep_neurons = 1
     block.compute_optimal_updates(maximum_added_neurons=keep_neurons)
 
-    # Sub select the optimal added parameters
-    # block.sub_select_optimal_added_parameters(3)
+    print(block.eigenvalues)
+
+    # Training loss with the change
+    scaling_factor = 0.5
+    loss_with_extension = evaluate_with_extension(block, dataset, scaling_factor)
+    print(f'Training Loss with the change: {loss_with_extension}')
+    print(f'First order improvement: {block.first_order_improvement / (batch_size * num_batch)}')
+    print(f'Zero-th order improvement: {training_loss_after - loss_with_extension}')
 
     # Apply the change
+    print('Apply the change')
     block.apply_change()
 
-    # Get the first order improvement
-    print(block.first_order_improvement)
+    # Delete the update
+    print('Delete the update')
+    block.delete_update()
+    block.reset_computation()
+
+    # Training loss after the change
+    training_loss_after_change = evaluate(block, dataset)
+    print(f'Training Loss after the change: {training_loss_after_change}')
+    print(f'Zero-th order improvement: {training_loss_after - training_loss_after_change}')
+
+    print(block)
+
+    # assert loss_with_extension == training_loss_after_change
+
+    # Tolerance for floating-point comparisons
+    tolerance = 1e-6  # Adjust this value based on the required precision
+
+    # Assert the two values are "close enough" within the tolerance
+    assert torch.isclose(
+        loss_with_extension,
+        training_loss_after_change,
+        atol=tolerance
+    ), (
+        f"Loss with extension ({loss_with_extension}) "
+        f"and training loss after change ({training_loss_after_change}) "
+        f"are not close enough. (Absolute difference: {torch.abs(loss_with_extension - training_loss_after_change)})"
+    )
