1697ce16ea28a08bcc5b00ec910e65f84cc0ed21
streethagore <srivaud972@gmail.com>
2025-01-12 00:45:19 +0100

    Implementing GrowingResidualBlock

--- scripts/mlp_launcher.sh
+++ scripts/mlp_launcher.sh
@@ -8,15 +8,15 @@ hidden_size=1
 
 # optimization arguments
 weight_decay=0
+epoch_per_growth=1
+selection_method='none'
 
 # growth arguments
-for nb_hidden_layer in 1; do
-  for epoch_per_growth in -1; do
-    for selection_method in 'none'; do
-      command="scripts/mlp_run.sh $nb_hidden_layer $hidden_size $weight_decay $epoch_per_growth $selection_method"
-      echo $command
+for hidden_size in 1 2 4 8 16 32 64; do
+  for epoch_per_growth in -1 0 1 2 4 6; do
+    command="scripts/mlp_run.sh $nb_hidden_layer $hidden_size $weight_decay $epoch_per_growth $selection_method"
+    echo $command
 #      sbatch --gres=gpu:1 --time=01:45:00 $command
-      eval "$command"
-    done
+    eval "$command"
   done
 done
