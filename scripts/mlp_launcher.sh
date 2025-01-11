672068f6375b9aae140a44fb037778dae669dc11
streethagore <srivaud972@gmail.com>
2025-01-12 00:51:24 +0100

    Implementing GrowingResidualBlock

--- scripts/mlp_launcher.sh
+++ scripts/mlp_launcher.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-mkdir -p slurm
+# Constants for growth parameters
+growth_hidden_sizes=(1 2 4 8 16 32 64)
+growth_epochs=(-1 0 1 2 4 6)
 
-# model arguments
-nb_hidden_layer=1
-hidden_size=1
+# Function to create the slurm directory
+setup_environment() {
+  mkdir -p slurm
+}
 
-# optimization arguments
-weight_decay=0
-epoch_per_growth=1
-selection_method='none'
+# Function to execute the batch jobs
+run_jobs() {
+  local nb_hidden_layer=1
+  local weight_decay=0
+  local selection_method='none'
 
-# growth arguments
-for hidden_size in 1 2 4 8 16 32 64; do
-  for epoch_per_growth in -1 0 1 2 4 6; do
-    command="scripts/mlp_run.sh $nb_hidden_layer $hidden_size $weight_decay $epoch_per_growth $selection_method"
-    echo $command
-#      sbatch --gres=gpu:1 --time=01:45:00 $command
-    eval "$command"
+  for hidden_size in "${growth_hidden_sizes[@]}"; do
+    for epoch_per_growth in "${growth_epochs[@]}"; do
+      local command="scripts/mlp_run.sh $nb_hidden_layer $hidden_size $weight_decay $epoch_per_growth $selection_method"
+      echo $command
+      sbatch --gres=gpu:1 --time=01:45:00 $command
+    done
   done
-done
+}
+
+# Main execution
+setup_environment
+run_jobs
