f12980e3fcaf9ecc15bfd05bf5bce969907c292f
streethagore <srivaud972@gmail.com>
2025-01-12 20:53:22 +0100

    Implementing GrowingResidualBlock

--- scripts/mlp_launcher.sh
+++ scripts/mlp_launcher.sh
@@ -12,7 +12,7 @@ setup_environment() {
 # Function to execute the batch jobs
 run_jobs() {
   local nb_hidden_layer=1
-  local weight_decay=0.0
+  local weight_decay=0.1
   local selection_method='none'
 
   for hidden_size in "${hidden_size_list[@]}"; do
