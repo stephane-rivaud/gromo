93f6f1ae058fe388d469f1c1aad6c9a058253ee9
streethagore <srivaud972@gmail.com>
2025-01-13 16:36:35 +0100

    Implementing GrowingResidualBlock

--- scripts/data_transfer_script.sh
+++ scripts/data_transfer_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Rsync command with a smooth progress bar using 'pv'
-rsync -azh --info=progress2 strivaud@titanic.saclay.inria.fr:/home/tau/strivaud/gromo/logs /Users/strivaud/PycharmProjects/gromo
+rsync -azh --info=progress2 strivaud@titanic.saclay.inria.fr:/home/tau/strivaud/gromo/logs/mlp_run /Users/strivaud/PycharmProjects/gromo/logs
