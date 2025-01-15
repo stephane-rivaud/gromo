b750a60736c8df198a10cf40e7dd91e5dd574ea6
streethagore <srivaud972@gmail.com>
2025-01-15 22:40:44 +0100

    Implementing GrowingResidualBlock

--- scripts/titanic_launcher.sh
+++ scripts/titanic_launcher.sh
@@ -20,11 +20,12 @@ run_jobs() {
 
   for weight_decay in "${weight_decay_list[@]}"; do
     for num_blocks in "${num_block_list[@]}"; do
-    for hidden_size in "${hidden_size_list[@]}"; do
-      for epoch_per_growth in "${epoch_per_growth_list[@]}"; do
-        local command="scripts/residual_mlp_run.sh $num_blocks $hidden_size $weight_decay $epoch_per_growth $selection_method"
-        echo $command
-        sbatch --gres=gpu:1 --time=01:45:00 $command
+      for hidden_size in "${hidden_size_list[@]}"; do
+        for epoch_per_growth in "${epoch_per_growth_list[@]}"; do
+          local command="scripts/residual_mlp_run.sh $num_blocks $hidden_size $weight_decay $epoch_per_growth $selection_method"
+          echo $command
+          sbatch --gres=gpu:1 --time=01:45:00 $command
+        done
       done
     done
   done
