84829e80618acfd7b7aa79aaf1027ddc658c4231
streethagore <srivaud972@gmail.com>
2025-01-15 23:26:40 +0100

    Implementing GrowingResidualBlock

--- scripts/titanic_launcher.sh
+++ scripts/titanic_launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Constants for growth parameters
-num_block_list=(1)
+num_block_list=(2 3 4 5)
 hidden_size_list=(1 4 16 64 256 1024)
 epoch_per_growth_list=(8 4 2 1 0)
 weight_decay_list=(0.0)
@@ -12,16 +12,22 @@ setup_environment() {
 }
 
 # Function to execute the batch jobs
-run_jobs() {
-  local selection_method='none'
 
+run_jobs() {
   for weight_decay in "${weight_decay_list[@]}"; do
     for num_blocks in "${num_block_list[@]}"; do
       for hidden_size in "${hidden_size_list[@]}"; do
         for epoch_per_growth in "${epoch_per_growth_list[@]}"; do
-          local command="scripts/residual_mlp_run.sh $num_blocks $hidden_size $weight_decay $epoch_per_growth $selection_method"
-          echo $command
-          sbatch --gres=gpu:1 --time=01:45:00 $command
+          if [ "$num_blocks" == 1 ]; then
+            selection_method=('none')
+          else
+            selection_method=('fo' 'none')
+          fi
+          for selection_method in "${selection_method[@]}"; do
+            local command="scripts/residual_mlp_run.sh $num_blocks $hidden_size $weight_decay $epoch_per_growth $selection_method"
+            echo $command
+            sbatch --gres=gpu:1 --time=01:45:00 $command
+          done
         done
       done
     done
