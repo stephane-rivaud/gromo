04ea6a12f8527ab749134ef9a824c17959387870
streethagore <srivaud972@gmail.com>
2025-01-16 19:10:59 +0100

    Implementing GrowingResidualBlock

--- scripts/margaret_launcher.sh
+++ scripts/margaret_launcher.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # Constants for growth parameters
-hidden_size_list=(1024)
+#num_block_list=(1 2 4 6)
+#num_features=(256 512 1024)
+#hidden_size_list=(32 64 128)
+#epoch_per_growth_list=(-1 8 4)
+#weight_decay_list=(0.0 0.01)
+
+num_block_list=(10)
+num_features=(1024)
+hidden_size_list=(256)
 epoch_per_growth_list=(-1)
+weight_decay_list=(0.0 0.01)
 
 # Function to create the slurm directory
 setup_environment() {
@@ -10,16 +19,24 @@ setup_environment() {
 }
 
 # Function to execute the batch jobs
-run_jobs() {
-  local nb_hidden_layer=20
-  local weight_decay=0.0
-  local selection_method='none'
 
-  for hidden_size in "${hidden_size_list[@]}"; do
-    for epoch_per_growth in "${epoch_per_growth_list[@]}"; do
-      local command="scripts/mlp_run.sh $nb_hidden_layer $hidden_size $weight_decay $epoch_per_growth $selection_method"
-      echo $command
-      sbatch --partition tau --gres=gpu:1 --time=01:45:00 $command
+run_jobs() {
+  for weight_decay in "${weight_decay_list[@]}"; do
+    for hidden_size in "${hidden_size_list[@]}"; do
+      for num_features in "${num_features[@]}"; do
+        for num_blocks in "${num_block_list[@]}"; do
+          for epoch_per_growth in "${epoch_per_growth_list[@]}"; do
+            if [ "$num_blocks" == 1 ]; then
+              selection_method='none'
+            else
+              selection_method='fo'
+            fi
+            local command="scripts/residual_mlp_run.sh $num_blocks $num_features $hidden_size $weight_decay $epoch_per_growth $selection_method"
+            echo $command
+            sbatch --partition tau --gres=gpu:1 --time=01:45:00 $command
+          done
+        done
+      done
     done
   done
 }
