a2ef17623f68dff2777f2017318bffad26ee9104
streethagore <srivaud972@gmail.com>
2025-01-22 21:28:35 +0100

    Refactoring and adding datasets

--- scripts/margaret_launcher.sh
+++ scripts/margaret_launcher.sh
@@ -7,12 +7,9 @@
 #epoch_per_growth_list=(-1 8 4)
 #weight_decay_list=(0.0 0.0005)
 
-num_block_list=(4)
-num_features=(64 128)
-hidden_size_list=(16 32 64 128)
+num_block_list=(8)
 epoch_per_growth_list=(-1)
-weight_decay_list=(0.01 0.001 0.0001)
-dropout_list=(0.5 0.3)
+weight_decay_list=(5e-5)
 
 # Function to create the slurm directory
 setup_environment() {
@@ -22,23 +19,17 @@ setup_environment() {
 # Function to execute the batch jobs
 
 run_jobs() {
-  for hidden_size in "${hidden_size_list[@]}"; do
-    for num_features in "${num_features[@]}"; do
-      for num_blocks in "${num_block_list[@]}"; do
-        for epoch_per_growth in "${epoch_per_growth_list[@]}"; do
-          for weight_decay in "${weight_decay_list[@]}"; do
-            for dropout in "${dropout_list[@]}"; do
-              if [ "$num_blocks" == 1 ]; then
-                selection_method='none'
-              else
-                selection_method='fo'
-              fi
-              local command="scripts/mlp_mixer_run.sh $num_blocks $num_features $hidden_size $weight_decay $epoch_per_growth $selection_method $dropout"
-              echo $command
-              sbatch --partition tau --gres=gpu:1 --time=01:45:00 $command
-            done
-          done
-        done
+  for num_blocks in "${num_block_list[@]}"; do
+    for epoch_per_growth in "${epoch_per_growth_list[@]}"; do
+      for weight_decay in "${weight_decay_list[@]}"; do
+        if [ "$num_blocks" == 1 ]; then
+          selection_method='none'
+        else
+          selection_method='fo'
+        fi
+        local command="scripts/mlp_mixer_run.sh $num_blocks $weight_decay $epoch_per_growth $selection_method"
+        echo $command
+        sbatch --partition tau --gres=gpu:1 --time=01:45:00 $command
       done
     done
   done
