200577587757d2f66570b241f2603b48d28cba2c
streethagore <srivaud972@gmail.com>
2025-01-22 21:33:04 +0100

    Refactoring and adding datasets

--- scripts/mlp_mixer_run.sh
+++ scripts/mlp_mixer_run.sh
@@ -100,6 +100,7 @@ optimizer="adamw"
 lr=0.01
 #weight_decay=0
 dropout=0.0
+scheduler="cosine"
 
 command="${command} --batch-size $batch_size --optimizer $optimizer --lr $lr --weight-decay $weight_decay --dropout $dropout"
 
